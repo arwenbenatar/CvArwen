bd1868ed4974d59e6bb91bb8c723304fd59a79c5
Arwen Benatar <benatar.arwen@gmail.com>
2025-04-10 13:45:45 +0200

    fix: correction de la mise en page sur la page d'accueil

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   site: "https://arwenbenatar.github.io",
   base: "CvArwen",
   trailingSlash: "always",
-=======
->>>>>>> 4fe20cef4dcb15e16a4989c14e85e427bb85e66d
   integrations: [tailwind()],
 });
